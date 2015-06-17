14de37a70eaaa97f7528a15cbd07f661579bcbd6
Adam Bowles <bowlesa@aston.ac.uk>
2015-06-17 20:50:11 +0100

    test

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,7 +15,7 @@ Route::get('/', 'WelcomeController@index');
 
 Route::get('home', 'HomeController@index');
 
-Route::post('git_pull', 'GitController@pull');
+Route::post('git_pull', 'GitController@pull'); 
 
 Route::controllers([
 	'auth' => 'Auth\AuthController',
