4b909462a4dd3773a5b82db6ccf79febd62e2a72
Adam Bowles <bowlesa@aston.ac.uk>
2015-06-17 21:00:29 +0100

    allow git_pull route to ignore csrf token

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -5,16 +5,25 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
 
 class VerifyCsrfToken extends BaseVerifier {
 
-	/**
-	 * Handle an incoming request.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  \Closure  $next
-	 * @return mixed
-	 */
-	public function handle($request, Closure $next)
-	{
-		return parent::handle($request, $next);
-	}
+  private $openRoutes = ['git_pull'];
+
+  /**
+   * Handle an incoming request.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \Closure  $next
+   * @return mixed
+   */
+  public function handle($request, Closure $next)
+  {
+
+    foreach($this->openRoutes as $route) {
+      if ($request->is($route)) {
+        return $next($request);
+      }
+    }
+
+    return parent::handle($request, $next);
+  }
 
 }
