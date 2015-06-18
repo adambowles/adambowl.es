f0a0f3f8f0b4db9e950966c9cd81d4df29499d5d
Adam Bowles <bowlesa@aston.ac.uk>
2015-06-18 22:19:18 +0100

    test

--- app/Http/Controllers/GithubController.php
+++ app/Http/Controllers/GithubController.php
@@ -30,6 +30,27 @@ class GithubController extends Controller {
   */
   public function pull()
   {
+    $hookSecret = env('GITHUB_SECRET');
+
+    if (!isset($_SERVER['HTTP_X_HUB_SIGNATURE'])) {
+      abort(500, 'Secret key not supplied');
+    } elseif (!extension_loaded('hash')) {
+      abort(500, 'Missing \'hash\' extension');
+    }
+
+    list($algo, $hash) = explode('=', $_SERVER['HTTP_X_HUB_SIGNATURE'], 2) + array('', '');
+    if (!in_array($algo, hash_algos(), TRUE)) {
+      abort(500, 'Hash algorithm \'$algo\' is not supported.');
+    }
+
+    $rawPost = file_get_contents('php://input');
+    if ($hash !== hash_hmac($algo, $rawPost, $hookSecret)) {
+      abort(500, 'Hook secret does not match.');
+    }
+
+    return 'temp';
+
+
     if(!isset($_SERVER['HTTP_X_HUB_SIGNATURE'])) {
       abort(500, 'Secret key not supplied');
     }
