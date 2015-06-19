15880cc8b13618815527e99e76b8c9a94b8a791a
Adam Bowles <bowlesa@aston.ac.uk>
2015-06-19 16:19:46 +0100

    disable laravel example default route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,7 +13,7 @@
 
 Route::get('/', 'WelcomeController@index');
 
-Route::get('home', 'HomeController@index');
+// Route::get('home', 'HomeController@index');
 
 Route::post('github_pull', 'GithubController@pull');
 
