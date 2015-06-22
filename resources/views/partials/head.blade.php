213691ec00ec94b15626ad8f619d6b9465591f57
Adam Bowles <contact@adambowl.es>
2015-06-22 10:03:44 +0100

    port js to local

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -19,5 +19,5 @@
 <!--[if lt IE 9]>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
-  <script src="http://webthemez.com/demo/john-bootstrap-one-page-html5-free-resume-template/js/html5element.js"></script>
+  <script src="{{ asset('js/html5element.css') }}"></script>
 <![endif]-->
