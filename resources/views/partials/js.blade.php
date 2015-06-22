9c06726599e7e69be3a9926291c2c243bf480d0b
Adam Bowles <contact@adambowl.es>
2015-06-22 11:05:13 +0100

    Update js.blade.php

--- resources/views/partials/js.blade.php
+++ resources/views/partials/js.blade.php
@@ -1,10 +1,10 @@
 <script type="text/javascript" src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
 <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="http://webthemez.com/demo/john-bootstrap-one-page-html5-free-resume-template/js/jquery-scrolltofixed.js"></script>
-<script type="text/javascript" src="http://webthemez.com/demo/john-bootstrap-one-page-html5-free-resume-template/js/jquery.nav.js"></script>
-<script type="text/javascript" src="http://webthemez.com/demo/john-bootstrap-one-page-html5-free-resume-template/js/jquery.easing.1.3.js"></script>
-<script type="text/javascript" src="http://webthemez.com/demo/john-bootstrap-one-page-html5-free-resume-template/js/jquery.isotope.js"></script>
-<script type="text/javascript" src="http://webthemez.com/demo/john-bootstrap-one-page-html5-free-resume-template/js/fancybox/jquery.fancybox.pack.js"></script>
-<script type="text/javascript" src="http://webthemez.com/demo/john-bootstrap-one-page-html5-free-resume-template/js/wow.js"></script>
-<script type="text/javascript" src="http://webthemez.com/demo/john-bootstrap-one-page-html5-free-resume-template/js/custom.js"></script>
+<script type="text/javascript" src="{{ asset('js/jquery.scrolltofixed.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/jquery.nav.js') }}"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+<script type="text/javascript" src="{{ asset('js/jquery.isotope.js') }}"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
+<script type="text/javascript" src="{{ asset('js/wow.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/custom.js') }}"></script>
 <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery.perfect-scrollbar/0.6.2/js/min/perfect-scrollbar.jquery.min.js"></script>
