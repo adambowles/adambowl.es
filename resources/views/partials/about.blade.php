ac4c3b1cc1ed806f493b444df4a558ff2b766619
Adam Bowles <contact@adambowl.es>
2015-06-22 10:43:19 +0100

    replace placeholder

--- resources/views/partials/about.blade.php
+++ resources/views/partials/about.blade.php
@@ -5,7 +5,9 @@
       <h6>A person who loves Nature and It's creations</h6>
       <div class="inner_section">
         <div class="row">
-          <div class="col-md-6"> <img class="img-responsive" src="holder.js/400x400" align=""> </div>
+          <div class="col-md-6">
+            <img class="img-responsive" src="http://placehold.it/400x400" align="">
+          </div>
           <div class="col-md-6">
             <h3>My Passion</h3>
             <p>Lorem ipsum dolor sit amet, quo meis voluptaria, erant bonorum albucius et per, ei placerat eu, te eos porro veniam. An everti maiorum detracto mea. Eu eos dicam voluptaria, erant bonorum albucius et per, ei sapientem accommodare est. Saepe dolorum constituam ei vel. Te sit malorum ceteros repudiandae, ne tritani adipisci vis.</p>
