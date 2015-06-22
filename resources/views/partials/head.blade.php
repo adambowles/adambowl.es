1742d87c293931fe2e2f8ba9b7838911bf40433a
Adam Bowles <contact@adambowl.es>
2015-06-22 11:08:55 +0100

    Update head.blade.php

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -15,9 +15,3 @@
     font-family: 'San Francisco Text', sans-serif;
   }
 </style>
-
-<!--[if lt IE 9]>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
-  <script src="{{ asset('js/html5element.css') }}"></script>
-<![endif]-->
