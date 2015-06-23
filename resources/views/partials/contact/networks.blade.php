cc3a5dd3af161e3c4cd8cd151899eb6acff77249
Adam Bowles <contact@adambowl.es>
2015-06-23 14:50:37 +0100

    Create networks.blade.php

---
+++ resources/views/partials/contact/networks.blade.php
@@ -0,0 +1,6 @@
+<ul class="social_links">
+  <li class="twitter animated bounceIn wow delay-02s"><a href="http://www.twitter.com/adambowles"><i class="fa fa-twitter"></i></a></li>
+  <li class="facebook animated bounceIn wow delay-03s"><a href="http://www.facebook.com/adambowles1"><i class="fa fa-facebook"></i></a></li>
+  <li class="pinterest animated bounceIn wow delay-04s"><a href="http://www.github.com/adambowles"><i class="fa fa-pinterest"></i></a></li>
+  <li class="github animated bounceIn wow delay-05s"><a href="http://www.github.com/adambowles"><i class="fa fa-github"></i></a></li>
+</ul>
