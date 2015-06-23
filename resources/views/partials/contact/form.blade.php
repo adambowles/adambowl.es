9a1243abf2f6fa3b9056613f4d340e5f464c278e
Adam Bowles <contact@adambowl.es>
2015-06-23 22:46:44 +0100

    make submit button bigger

--- resources/views/partials/contact/form.blade.php
+++ resources/views/partials/contact/form.blade.php
@@ -3,5 +3,5 @@
   <input class="input-text" type="email" name="email" placeholder="Email">
   <input class="input-text" type="tel" name="phoneNumber" placeholder="Phone number">
   <textarea class="input-text text-area" name="message" placeholder="Message"></textarea>
-  <input class="input-btn" type="submit" value="send message">
+  <input class="input-btn btn-block" type="submit" value="send message">
 </div>
