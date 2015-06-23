a6ffa2c8c19c9f1ddf62f8667b430ddcdb23e016
Adam Bowles <contact@adambowl.es>
2015-06-23 14:53:09 +0100

    Create form.blade.php

---
+++ resources/views/partials/contact/form.blade.php
@@ -0,0 +1,7 @@
+<div class="form">
+  <input class="input-text" type="text" name="name" placeholder="Your name">
+  <input class="input-text" type="email" name="email" placeholder="Your email">
+  <input class="input-text" type="tel" name="phoneNumber" placeholder="Your phone number">
+  <textarea class="input-text text-area" name="message" placeholder="Your message"></textarea>
+  <input class="input-btn" type="submit" value="send message">
+</div>
