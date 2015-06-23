4907e76b8666ac552b42f856afb15ba7ebcb6993
Adam Bowles <contact@adambowl.es>
2015-06-23 21:47:56 +0100

    remove extraneous text

--- resources/views/partials/skills/bar.blade.php
+++ resources/views/partials/skills/bar.blade.php
@@ -2,6 +2,6 @@
   <div class="progress-bar" role="progressbar" aria-valuenow="{{ $progress or '100' }}" aria-valuemin="0" aria-valuemax="{{ $progress or '100' }}" style="width: {{ $progress or '100' }}%">
     <span class="sr-only">{{ $progress or '100' }}% Complete</span>
   </div>
-  <span class="progress-type">JavaScript / jQuery{{ $name or 'Skill name' }}</span>
+  <span class="progress-type">{{ $name or 'Skill name' }}</span>
   <span class="progress-completed">{{ $progress or '100' }}%</span>
 </div>
