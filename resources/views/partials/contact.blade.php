0dc4dbe87233384e146ab458762fedb5730f0379
Adam Bowles <contact@adambowl.es>
2015-06-23 14:47:51 +0100

    Create contact.blade.php

---
+++ resources/views/partials/contact.blade.php
@@ -0,0 +1,42 @@
+  <div class="container">
+    <section class="page_section contact" id="contact">
+      <div class="contact_section">
+        <h2>Contact me</h2>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-4 wow fadeInLeft">
+          <div class="contact_info">
+            <div class="detail">
+              <h4>Email me</h4>
+              <p><a href="mailto:contact@adambowl.es">contact@adambowl.es</a></p>
+            </div>
+            <div class="detail">
+              <h4>Call me</h4>
+              <p><tel>07584 356 713</tel></p>
+            </div>
+              <div class="detail">
+                <h4>Write to me</h4>
+                <p>277 Droitwich Road, Worcester, WR3 7SU</p>
+              </div>
+          </div>
+
+          <ul class="social_links">
+            <li class="twitter animated bounceIn wow delay-02s"><a href="http://www.twitter.com/adambowles"><i class="fa fa-twitter"></i></a></li>
+            <li class="facebook animated bounceIn wow delay-03s"><a href="http://www.facebook.com/adambowles1"><i class="fa fa-facebook"></i></a></li>
+            <li class="pinterest animated bounceIn wow delay-04s"><a href="http://www.github.com/adambowles"><i class="fa fa-pinterest"></i></a></li>
+            <li class="github animated bounceIn wow delay-05s"><a href="http://www.github.com/adambowles"><i class="fa fa-github"></i></a></li>
+          </ul>
+        </div>
+        <div class="col-lg-8 wow fadeInLeft delay-06s">
+          <div class="form">
+            <input class="input-text" type="text" name="name" placeholder="Your name">
+            <input class="input-text" type="email" name="email" placeholder="Your email">
+            <input class="input-text" type="tel" name="phoneNumber" placeholder="Your phone number">
+            <textarea class="input-text text-area" name="message" placeholder="Your message"></textarea>
+            <input class="input-btn" type="submit" value="send message">
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
