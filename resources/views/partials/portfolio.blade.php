7e3bcb181f295c419d57f9bb10f0416e41154577
Adam Bowles <contact@adambowl.es>
2015-06-23 10:51:09 +0100

    extract portfolio item

--- resources/views/partials/portfolio.blade.php
+++ resources/views/partials/portfolio.blade.php
@@ -42,20 +42,7 @@
     
     <div class="isotope fadeInLeft animated wow grid" style="position: relative; overflow: hidden; height: 480px;" id="portfolio_wrapper">
       
-      <figure style="position: absolute; left: 0px; top: 0px; transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1); width: 337px; opacity: 1;" class="mobile portfolio-item one-four isotope-item effect-oscar">
-
-        <div class="portfolio_img">
-          <img src="http://lorempixel.com/output/city-q-c-825-550-3.jpg">
-        </div>
-        <figcaption>
-          <div>
-            <a href="http://lorempixel.com/output/city-q-c-825-550-3.jpg" class="fancybox">
-              <h2>Lorem</h2>
-              <p>Morbi pharetra turpis sit amet est convallis facilisis</p>
-            </a>
-          </div>
-        </figcaption>
-      </figure>
+      @include('partials.portfolio.item')
       
       <figure style="position: absolute; left: 0px; top: 0px; transform: translate3d(337px, 0px, 0px) scale3d(1, 1, 1); width: 337px; opacity: 1;" class="development portfolio-item one-four isotope-item effect-oscar">
         <div class="portfolio_img">
