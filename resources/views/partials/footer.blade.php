c688770b1ff3ce9c00648fdf75243820d8916b99
Adam Bowles <contact@adambowl.es>
2015-06-23 14:44:58 +0100

    Update footer.blade.php

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,8 +2,7 @@
   <div class="container">
     <section class="page_section contact" id="contact">
       <div class="contact_section">
-        <h2>Contact Me</h2>
-        <h6>Get in touch!</h6>
+        <h2>Contact me</h2>
       </div>
 
       <div class="row">
@@ -32,9 +31,10 @@
         </div>
         <div class="col-lg-8 wow fadeInLeft delay-06s">
           <div class="form">
-            <input class="input-text" type="text" name="" placeholder="Your name">
-            <input class="input-text" type="text" name="" placeholder="Your email">
-            <textarea class="input-text text-area" cols="0" rows="0" placeholder="Your message"></textarea>
+            <input class="input-text" type="text" name="name" placeholder="Your name">
+            <input class="input-text" type="email" name="email" placeholder="Your email">
+            <input class="input-text" type="tel" name="phoneNumber" placeholder="Your phone number">
+            <textarea class="input-text text-area" name="message" placeholder="Your message"></textarea>
             <input class="input-btn" type="submit" value="send message">
           </div>
         </div>
