562f68f8d2a743f342f185ef38c8f48b2f0e6368
Adam Bowles <contact@adambowl.es>
2015-06-23 10:54:21 +0100

    Update item.blade.php

--- resources/views/partials/portfolio/item.blade.php
+++ resources/views/partials/portfolio/item.blade.php
@@ -1,12 +1,12 @@
 <figure style="position: absolute; left: 0px; top: 0px; transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1); width: 337px; opacity: 1;" class="mobile portfolio-item one-four isotope-item effect-oscar">
   <div class="portfolio_img">
-    <img src="http://lorempixel.com/output/city-q-c-825-550-3.jpg">
+    <img src="{{ $backgroundURL }}">
   </div>
   <figcaption>
     <div>
-      <a href="http://lorempixel.com/output/city-q-c-825-550-3.jpg" class="fancybox">
-        <h2>Lorem</h2>
-        <p>Morbi pharetra turpis sit amet est convallis facilisis</p>
+      <a href="{{ $backgroundURL }}" class="fancybox">
+        <h2>{{ $title }}</h2>
+        <p>{{ $description }}</p>
       </a>
     </div>
   </figcaption>
