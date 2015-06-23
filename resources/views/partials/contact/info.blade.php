c214e2464d5c6f90fd2bcb030fc11f6ea3aa6af5
Adam Bowles <contact@adambowl.es>
2015-06-23 14:52:31 +0100

    Create info.blade.php

---
+++ resources/views/partials/contact/info.blade.php
@@ -0,0 +1,14 @@
+<div class="contact_info">
+  <div class="detail">
+    <h4>Email me</h4>
+    <p><a href="mailto:contact@adambowl.es">contact@adambowl.es</a></p>
+  </div>
+  <div class="detail">
+    <h4>Call me</h4>
+    <p><tel>07584 356 713</tel></p>
+  </div>
+  <div class="detail">
+    <h4>Write to me</h4>
+    <p>277 Droitwich Road, Worcester, WR3 7SU</p>
+  </div>
+</div>
