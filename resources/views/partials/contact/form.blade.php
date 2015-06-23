abe44e6dc0f054c15461ae6139267301460023c6
Adam Bowles <contact@adambowl.es>
2015-06-23 21:35:12 +0100

    remove "your" from fields

--- resources/views/partials/contact/form.blade.php
+++ resources/views/partials/contact/form.blade.php
@@ -1,7 +1,7 @@
 <div class="form">
-  <input class="input-text" type="text" name="name" placeholder="Your name">
-  <input class="input-text" type="email" name="email" placeholder="Your email">
-  <input class="input-text" type="tel" name="phoneNumber" placeholder="Your phone number">
-  <textarea class="input-text text-area" name="message" placeholder="Your message"></textarea>
+  <input class="input-text" type="text" name="name" placeholder="Name">
+  <input class="input-text" type="email" name="email" placeholder="Email">
+  <input class="input-text" type="tel" name="phoneNumber" placeholder="Phone number">
+  <textarea class="input-text text-area" name="message" placeholder="Message"></textarea>
   <input class="input-btn" type="submit" value="send message">
 </div>
