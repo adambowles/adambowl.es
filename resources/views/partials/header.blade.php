6065ebb6b8d9cca4161ba62c7d1caf42ada9e0ea
Adam Bowles <contact@adambowl.es>
2015-06-23 21:57:20 +0100

    spelling fix

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,7 +4,7 @@
     <div class="top_left_cont zoomIn wow animated" id="logo">
       <img class="portrait" src="http://webthemez.com/demo/john-bootstrap-one-page-html5-free-resume-template/img/guy.jpg" alt="">
 
-      <h2>Hello! I am Adam Bowles</h2>
+      <h2>Hello! I'm Adam Bowles</h2>
       <h2>Software Engineer by trade, weekend warrior rock climber</h2>
     </div>
 
