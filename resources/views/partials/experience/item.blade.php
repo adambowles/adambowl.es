a17e3af8cb9d1edd1444e84e4f975b339407bd53
Adam Bowles <contact@adambowl.es>
2015-06-23 14:04:33 +0100

    rename role -> job title

--- resources/views/partials/experience/item.blade.php
+++ resources/views/partials/experience/item.blade.php
@@ -4,7 +4,7 @@
       <p>{{ $date or 'Jan' }}</p>
     </div>
     <h2 class="news-title">{{ $company or 'Company' }}</h2>
-    <span>{{ $role or 'Role' }}</span>
+    <span>{{ $jobTitle or 'Job title' }}</span>
     <p>{{ $specificDates or 'Month YYYY - Month YYYY (X months/years)' }}</p>
     <p>{{ $jobDescription or 'Lorm ipsum dolor sit amet' }}</p>
     <a class="read-more" href="#">Read More</a>
