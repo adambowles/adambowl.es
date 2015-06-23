b5c30168d880ce49862b3a3b1505df30c35e0613
Adam Bowles <contact@adambowl.es>
2015-06-23 22:22:06 +0100

    add item classes

--- resources/views/partials/portfolio.blade.php
+++ resources/views/partials/portfolio.blade.php
@@ -39,17 +39,17 @@
         </li>
       </ul>
     </div>
-    
+
     <div class="isotope grid" style="position: relative; overflow: hidden; height: 480px;" id="portfolio_wrapper">
-      
-      @include('partials.portfolio.item', ['title' => 'Lorem', 'description' => 'Morbi pharetra turpis sit amet est convallis facilisis',  'backgroundURL' => 'http://lorempixel.com/output/city-q-c-825-550-3.jpg'])
-      @include('partials.portfolio.item', ['title' => 'Ipsum', 'description' => 'Fusce tristique, erat sed vehicula semper, leo ipsum',  'backgroundURL' => 'http://lorempixel.com/output/city-q-c-825-550-5.jpg'])
-      @include('partials.portfolio.item', ['title' => 'Dolor', 'description' => 'Quisque varius, sem vel malesuada rutrum, libero libero',  'backgroundURL' => 'http://lorempixel.com/output/transport-q-c-825-550-10.jpg'])
-      @include('partials.portfolio.item', ['title' => 'Sit', 'description' => 'Phasellus vel urna ut felis commodo tincidunt sit amet quis leo',  'backgroundURL' => 'http://lorempixel.com/output/transport-q-c-825-550-8.jpg'])
-      @include('partials.portfolio.item', ['title' => 'Amet', 'description' => 'Aliquam pretium consectetur dolor, quis lobortis ligula vestibulum non',  'backgroundURL' => 'http://lorempixel.com/output/technics-q-c-825-550-8.jpg'])
-      @include('partials.portfolio.item', ['title' => 'Consectetur', 'description' => 'Nunc ultricies quam nec turpis luctus vestibulum eu at purus',  'backgroundURL' => 'http://lorempixel.com/output/technics-q-c-825-550-2.jpg'])
-      @include('partials.portfolio.item', ['title' => 'Adipiscing', 'description' => 'Quisque a justo quis arcu feugiat fermentum in vel eros',  'backgroundURL' => 'http://lorempixel.com/output/technics-q-c-825-550-7.jpg'])
-      @include('partials.portfolio.item', ['title' => 'Elit', 'description' => 'Fusce in elit tincidunt turpis tempor viverra',  'backgroundURL' => 'http://lorempixel.com/output/sports-q-c-825-550-1.jpg'])
+
+      @include('partials.portfolio.item', ['title' => 'Lorem', 'description' => 'Morbi pharetra turpis sit amet est convallis facilisis',  'backgroundURL' => 'http://lorempixel.com/output/city-q-c-825-550-3.jpg', 'class' => 'design'])
+      @include('partials.portfolio.item', ['title' => 'Ipsum', 'description' => 'Fusce tristique, erat sed vehicula semper, leo ipsum',  'backgroundURL' => 'http://lorempixel.com/output/city-q-c-825-550-5.jpg', 'class' => 'development'])
+      @include('partials.portfolio.item', ['title' => 'Dolor', 'description' => 'Quisque varius, sem vel malesuada rutrum, libero libero',  'backgroundURL' => 'http://lorempixel.com/output/transport-q-c-825-550-10.jpg', 'class' => 'web'])
+      @include('partials.portfolio.item', ['title' => 'Sit', 'description' => 'Phasellus vel urna ut felis commodo tincidunt sit amet quis leo',  'backgroundURL' => 'http://lorempixel.com/output/transport-q-c-825-550-8.jpg', 'class' => 'mobile'])
+      @include('partials.portfolio.item', ['title' => 'Amet', 'description' => 'Aliquam pretium consectetur dolor, quis lobortis ligula vestibulum non',  'backgroundURL' => 'http://lorempixel.com/output/technics-q-c-825-550-8.jpg', 'class' => 'design'])
+      @include('partials.portfolio.item', ['title' => 'Consectetur', 'description' => 'Nunc ultricies quam nec turpis luctus vestibulum eu at purus',  'backgroundURL' => 'http://lorempixel.com/output/technics-q-c-825-550-2.jpg', 'class' => 'development'])
+      @include('partials.portfolio.item', ['title' => 'Adipiscing', 'description' => 'Quisque a justo quis arcu feugiat fermentum in vel eros',  'backgroundURL' => 'http://lorempixel.com/output/technics-q-c-825-550-7.jpg', 'class' => 'web'])
+      @include('partials.portfolio.item', ['title' => 'Elit', 'description' => 'Fusce in elit tincidunt turpis tempor viverra',  'backgroundURL' => 'http://lorempixel.com/output/sports-q-c-825-550-1.jpg', 'class' => 'mobile'])
 
     </div>
   </div>
