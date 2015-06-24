0b6ae0ad532e4ee16e737da26f1c6ebfa84e7d65
Adam Bowles <contact@adambowl.es>
2015-06-24 09:00:11 +0100

    add linkedin icon

--- resources/views/partials/contact/networks.blade.php
+++ resources/views/partials/contact/networks.blade.php
@@ -1,11 +1,14 @@
 <div class="row text-center lead">
-  <div class="col-xs-4">
+  <div class="col-xs-3">
     <a href="http://www.twitter.com/adambowles"><i class="fa fa-twitter"></i></a>
   </div>
-  <div class="col-xs-4">
+  <div class="col-xs-3">
     <a href="http://www.facebook.com/adambowles1"><i class="fa fa-facebook"></i></a>
   </div>
-  <div class="col-xs-4">
+  <div class="col-xs-3">
+    <a href="https://www.linkedin.com/profile/view?id=419043210"><i class="fa fa-linkedin"></i></a>
+  </div>
+  <div class="col-xs-3">
     <a href="http://www.github.com/adambowles"><i class="fa fa-github"></i></a>
   </div>
 </div>
