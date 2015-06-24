73ae53e16636289a08327b8b97fe6fd58772654a
Adam Bowles <contact@adambowl.es>
2015-06-24 09:01:59 +0100

    changed linkedin to public link

--- resources/views/partials/contact/networks.blade.php
+++ resources/views/partials/contact/networks.blade.php
@@ -6,7 +6,7 @@
     <a href="http://www.facebook.com/adambowles1"><i class="fa fa-facebook"></i></a>
   </div>
   <div class="col-xs-3">
-    <a href="https://www.linkedin.com/profile/view?id=419043210"><i class="fa fa-linkedin"></i></a>
+    <a href="https://www.linkedin.com/pub/adam-bowles/b8/405/a36"><i class="fa fa-linkedin"></i></a>
   </div>
   <div class="col-xs-3">
     <a href="http://www.github.com/adambowles"><i class="fa fa-github"></i></a>
