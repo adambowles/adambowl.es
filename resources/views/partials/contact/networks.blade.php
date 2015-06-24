130b7b33f1b422fdb97536b87b52de441f6d24da
Adam Bowles <contact@adambowl.es>
2015-06-24 22:49:04 +0100

    social icons -> buttons

--- resources/views/partials/contact/networks.blade.php
+++ resources/views/partials/contact/networks.blade.php
@@ -1,14 +1,14 @@
 <div class="row text-center lead">
   <div class="col-xs-3">
-    <a href="http://www.twitter.com/adambowles"><i class="fa fa-twitter"></i></a>
+    <a class="btn social-btn" href="http://www.twitter.com/adambowles"><i class="fa fa-twitter"></i></a>
   </div>
   <div class="col-xs-3">
-    <a href="http://www.facebook.com/adambowles1"><i class="fa fa-facebook"></i></a>
+    <a class="btn social-btn" href="http://www.facebook.com/adambowles1"><i class="fa fa-facebook"></i></a>
   </div>
   <div class="col-xs-3">
-    <a href="https://www.linkedin.com/pub/adam-bowles/b8/405/a36"><i class="fa fa-linkedin"></i></a>
+    <a class="btn social-btn" href="https://www.linkedin.com/pub/adam-bowles/b8/405/a36"><i class="fa fa-linkedin"></i></a>
   </div>
   <div class="col-xs-3">
-    <a href="http://www.github.com/adambowles"><i class="fa fa-github"></i></a>
+    <a class="btn social-btn" href="http://www.github.com/adambowles"><i class="fa fa-github"></i></a>
   </div>
 </div>
