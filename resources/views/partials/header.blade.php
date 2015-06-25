792172a8f892d2c01afa52391f7f75aa43f6c427
Adam Bowles <contact@adambowl.es>
2015-06-25 19:19:29 +0100

    Update

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <header id="header">
   <div id="head">
 
-    <div class="top_left_cont " id="logo">
+    <div class="top_left_cont" id="logo">
       <img class="portrait" src="http://webthemez.com/demo/john-bootstrap-one-page-html5-free-resume-template/img/guy.jpg" alt="">
 
       <h2>Hello! I'm Adam Bowles</h2>
