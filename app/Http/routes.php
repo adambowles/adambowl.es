e39fc86176c1dce68a35fcab4a277a6513e17338
Adam Bowles <contact@adambowl.es>
2015-06-25 19:31:05 +0100

    add route for new bootstrap

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,6 +14,7 @@
 Route::get('/', 'WelcomeController@index');
 
 Route::get('/material', 'WelcomeController@material');
+Route::get('/bs2',      'WelcomeController@bootstrap2');
 
 // Route::get('home', 'HomeController@index');
 
