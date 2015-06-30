8b1cf955052774d67bd3d1406b130d1c85951c8d
Adam Bowles <contact@adambowl.es>
2015-06-30 21:49:28 +0100

    add font awesome and timeline

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,5 +1,6 @@
 <meta charset="utf-8">
 <title>Adam Bowles</title>
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
 <link href={{ asset('css/custom.css')}} rel="stylesheet">
 <link href={{ asset('css/sanfrancisco.css')}} rel="stylesheet">
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,10 +1,11 @@
-<section id="intro" data-speed="4" data-type="background">
+<section id="intro" data-speed="6" data-type="background">
   <div class="container">
     <div class="row text-center">
       <div class="col-xs-12">
         <h1> Some page title </h1>
         <p> This is a website. </p>
         <p> <a class="btn btn-primary" href="#">Tell me more</a> </p>
+        <i class="fa fa-mars-stroke-v"></i>
       </div>
     </div>
   </div>
