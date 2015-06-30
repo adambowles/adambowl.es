1f5929131421d78f2087225201c8820bd99afafa
Adam Bowles <contact@adambowl.es>
2015-06-30 17:00:06 +0100

    fixed remote reference to js file

--- resources/views/bs2.blade.php
+++ resources/views/bs2.blade.php
@@ -263,8 +263,8 @@
     <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
     <script src="http://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-    <!--script src="https://raw.githubusercontent.com/maciej-gurban/responsive-bootstrap-toolkit/master/dist/bootstrap-toolkit.min.js"></script-->
-    <script src="bootstrap-toolkit.min.js"></script>
+    <script src="https://raw.githubusercontent.com/maciej-gurban/responsive-bootstrap-toolkit/master/dist/bootstrap-toolkit.min.js"></script>
+    <!-- <script src="bootstrap-toolkit.min.js"></script> -->
 
     <script>
       /*------------------------------------------------------*/
