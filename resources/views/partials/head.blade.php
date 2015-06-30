508134927c699dea7e5954bb494d7623ee29864c
Adam Bowles <contact@adambowl.es>
2015-06-30 22:10:10 +0100

    allow font awesome to style icons
    silly mistake!

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,6 +1,6 @@
 <meta charset="utf-8">
 <title>Adam Bowles</title>
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 <link href={{ asset('css/custom.css')}} rel="stylesheet">
 <link href={{ asset('css/sanfrancisco.css')}} rel="stylesheet">
