ff3a0cf6b341422d03f8442a34798b49fcb1ac8d
Adam Bowles <contact@adambowl.es>
2015-07-03 15:04:51 +0100

    reorganise css/js

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -17,6 +17,6 @@
 
     @include('partials.footer')
 
-    @include('partials.js')
+    @include('partials.assets.js')
   </body>
 </html>
--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,6 +1,3 @@
 <meta charset="utf-8">
 <title>Adam Bowles</title>
-<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-<link href={{ asset('css/custom.css')}} rel="stylesheet">
-<link href={{ asset('css/sanfrancisco.css')}} rel="stylesheet">
+@include('partials.assets.css')
