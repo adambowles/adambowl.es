76896a9b911300c1904163224f1a813cfbf439eb
Adam Bowles <contact@adambowl.es>
2015-07-03 13:53:45 +0100

    generalise items

--- resources/views/partials/experience/item.blade.php
+++ resources/views/partials/experience/item.blade.php
@@ -2,11 +2,12 @@
   <div class="timeline-badge primary"><i class="fa fa-{{ $icon or 'check' }}"></i></div>
   <div class="timeline-panel">
     <div class="timeline-heading">
-      <h4 class="timeline-title">{{ $title or 'Title' }}</h4>
+      <h4 class="timeline-title">{{ $company or 'Company' }}</h4><br>
+      <h5 class="timeline-title">{{ $jobTitle or 'Job Title' }}</h5><br>
       <h6 class="timeline-title">{{ $dates or 'Dates' }}</h6>
     </div>
     <div class="timeline-body">
-      <p>{{ $description or 'Description' }}<p>
+      <p>{{ $description or 'Description description description description description description description description' }}<p>
     </div>
   </div>
 </li>
--- resources/views/partials/experience/timeline.blade.php
+++ resources/views/partials/experience/timeline.blade.php
@@ -1,8 +1,9 @@
 <ul class="timeline">
 
-    @include('partials.experience.item', ['title' => 'Job 1', 'dates' => 'Jan \'13 - June \'13', 'description' => 'Carles shabby chic bicycle rights Bushwick. Kale chips street art Odd Future synth tofu messenger bag.', 'icon' => 'building'])
-    @include('partials.experience.item', ['title' => 'Job 2', 'dates' => 'Jan \'13 - June \'13', 'description' => 'Hella PBR chambray blog, fap XOXO vinyl Intelligentsia. Cray McSweeney\'s 8-bit, church-key sustainable Etsy iPhone.', 'icon' => 'building'])
-    @include('partials.experience.item', ['title' => 'Job 3', 'dates' => 'Jan \'13 - June \'13', 'description' => 'Neutra church-key aesthetic meggings, swag plaid seitan skateboard 90\'s four dollar toast irony before they sold out ennui Williamsburg XOXO.', 'icon' => 'building'])
-    @include('partials.experience.item', ['title' => 'Job 4', 'dates' => 'Jan \'13 - June \'13', 'description' => 'McSweeney\'s migas tote bag, direct trade whatever twee Truffaut jean shorts Austin Shoreditch. Carles shabby chic bicycle rights Bushwick.', 'icon' => 'building'])
+    @include('partials.experience.item', ['company' => 'Roxel (UK Rocket Motors) Ltd', 'jobTitle' => 'IT Technician', 'dates' => 'Jun \'13 - present', 'description' => 'Carles shabby chic bicycle rights Bushwick. Kale chips street art Odd Future synth tofu messenger bag.', 'icon' => 'rocket'])
+    @include('partials.experience.item')
+    @include('partials.experience.item')
+    @include('partials.experience.item')
+    @include('partials.experience.item')
 
 </ul>
