be001a98213ab6bbc52e72c0c3f0ee60bf90a588
Adam Bowles <contact@adambowl.es>
2015-07-03 17:12:43 +0100

    organise assets folders

--- resources/views/partials/assets/css.blade.php
+++ resources/views/partials/assets/css.blade.php
@@ -1,3 +1,6 @@
+{{-- Libraries --}}
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+{{-- Custom --}}
 <link href={{ asset('css/custom.css')}} rel="stylesheet">
