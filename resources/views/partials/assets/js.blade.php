7005697fdede2a7714a1dc5bc7b913f4dc650326
Adam Bowles <contact@adambowl.es>
2015-07-05 19:48:44 +0100

    rename js with version

--- resources/views/partials/assets/js.blade.php
+++ resources/views/partials/assets/js.blade.php
@@ -2,7 +2,7 @@
 <script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
 <script src="//code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-<script src="{{ asset('js/bootstrap-toolkit.min.js') }}"></script>
+<script src="{{ asset('js/bootstrap-toolkit-2.5.0.min.js') }}"></script>
 
 {{-- Custom --}}
 <script src="{{ asset('js/custom.js') }}"></script>
