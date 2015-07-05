6e9e6912046dc4fcfcf4e1334a29be6394007aaa
Adam Bowles <contact@adambowl.es>
2015-07-05 20:52:00 +0100

    create link to gmaps

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -5,7 +5,7 @@
         <ul>
           <li>Adam Bowles</li>
           <li>Software Developer</li>
-          <li>West Midlands</li>
+          <li><a href="https://goo.gl/maps/Bt75L">West Midlands</a></li>
           <li><a href="mailto:contact@adambowl.es">contact@adambowl.es</a></li>
           <li><a href="tel:+447584356713">07584 356 713</a></li>
         </ul>
