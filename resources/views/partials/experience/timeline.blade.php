e521cab3f0b3e4bbc2133dff02fde53a1863cfec
Adam Bowles <contact@adambowl.es>
2015-07-05 20:52:12 +0100

    remove some placeholders

--- resources/views/partials/experience/timeline.blade.php
+++ resources/views/partials/experience/timeline.blade.php
@@ -2,8 +2,5 @@
 
     @include('partials.experience.item', ['company' => 'Roxel (UK Rocket Motors) Ltd', 'jobTitle' => 'IT Technician', 'dates' => 'Jun \'13 - present', 'description' => 'Carles shabby chic bicycle rights Bushwick. Kale chips street art Odd Future synth tofu messenger bag.', 'icon' => 'rocket'])
     @include('partials.experience.item')
-    @include('partials.experience.item')
-    @include('partials.experience.item')
-    @include('partials.experience.item')
 
 </ul>
