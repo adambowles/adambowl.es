a9a02eaeba691a80bde728f786d514974a397238
Adam Bowles <contact@adambowl.es>
2015-07-07 23:32:04 +0100

    reorganize imports
    include raw text meta tags first, so the favicon can't slow down loading

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,10 +1,10 @@
 <meta charset="utf-8">
 
-<link rel="icon" href="{{ asset('images/favicons/256.png') }}" type="image/x-icon" />
-
 <title>Adam Bowles</title>
 
 <meta name="description" content="Adam Bowles; PHP software developer from Birmingham UK">
 <meta name="author" content="Adam Bowles">
+
+<link rel="icon" href="{{ asset('images/favicons/256.png') }}" type="image/x-icon" />
 
 @include('partials.assets.css')
