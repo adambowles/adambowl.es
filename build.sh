e54281132c0f86a63c45c00a058cb6aa0a5ea77f
Adam Bowles <contact@adambowl.es>
2015-07-14 13:06:17 +0100

    add comments

--- build.sh
+++ build.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
+
+###
+# Build script
+###
+
+# Take the server down so people accessing the site will see a maintenance page
 php artisan down
+
+# Purge local changes that aren't gitignored
 git reset --hard
-git pull 2>&1 #re-route stderr into stdout
-#TODO perform maybe:
+
+# Pull from master repo, and combine stderr & stdout
+git pull 2>&1
+
+#//TODO perform maybe:
 #composer install
 #npm install
 #php artisan migrate
+
+# Put server back up
 php artisan up
