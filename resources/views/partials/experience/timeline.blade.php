d644b19510f4d41383c19d10765e86a22295d056
Adam Bowles <contact@adambowl.es>
2015-07-22 21:34:01 +0100

    job title update

--- resources/views/partials/experience/timeline.blade.php
+++ resources/views/partials/experience/timeline.blade.php
@@ -1,4 +1,4 @@
 <ul class="timeline">
-  @include('partials.experience.item', ['company' => 'Roxel (UK Rocket Motors) Ltd', 'jobTitle' => 'IT Technician', 'dates' => 'Jun \'13 - present', 'description' => 'Carles shabby chic bicycle rights Bushwick. Kale chips street art Odd Future synth tofu messenger bag.', 'icon' => 'rocket'])
+  @include('partials.experience.item', ['company' => 'Roxel (UK Rocket Motors) Ltd', 'jobTitle' => 'Business Analyst', 'dates' => 'Jun \'13 - present', 'description' => 'Carles shabby chic bicycle rights Bushwick. Kale chips street art Odd Future synth tofu messenger bag.', 'icon' => 'rocket'])
   @include('partials.experience.item', ['company' => 'Aston Active Software Engineering', 'jobTitle' => 'PHP Software Developer', 'dates' => 'Summer 2011', 'description' => 'Kale chips street art Odd Future synth tofu messenger bag. Carles shabby chic bicycle rights Bushwick.', 'icon' => 'graduation-cap'])
 </ul>
